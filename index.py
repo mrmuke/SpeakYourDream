0b2b27f005dbdd72efabb59f515d4f47aa3db78f
titut <conghoangbillnew@gmail.com>
2021-01-15 23:06:38 -0600

    words per minute + like

--- index.py
+++ index.py
@@ -2,14 +2,22 @@ from uploadToGSC import Upload_blob
 from speechToText import SpeechToText
 from textTone import getResponse
 import os
+import uuid
+import scipy.io.wavfile as wav
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gapi-chrome-extension-bece3debbebd.json"
 
 path = "C:/Workspace/SpeakYourDream/ted.wav"
-fullpath_name = path.split(".")[0].split("/")
-file_name = fullpath_name[len(fullpath_name)-1]
+(source_rate, source_total) = wav.read(path)
+totalTime = len(source_total)/float(source_rate)
+file_name = str(uuid.uuid1())
 
 Upload_blob(path, file_name)
 
 result = SpeechToText("gs://speakyourdream2086/" + file_name)
+noLikes = len(result.split(' like '))-1
+totalWords = len(result.split(' '))
+print(result)
+print("number of likes:", noLikes)
+print("words per minute", (totalWords/totalTime) * 60)
 print(getResponse(result))
