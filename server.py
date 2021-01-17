ed92ffffa7b2be10db4af84ea36851ccc62e50d5
Aaron Zhang <yunjian2000@yahoo.com>
2021-01-16 21:09:52 -0600

    recording

--- server.py
+++ server.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template, send_file, redirect, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-   return render_template('home.html')
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
